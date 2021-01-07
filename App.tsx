5652b41c5801c36937642a61a951025b9a5698ca
Arthur Cargnelli <arthur.cargnelli@gmail.com>
2021-01-07 16:11:33 +0530

    change userid , api key and parentId

--- App.tsx
+++ App.tsx
@@ -29,10 +29,10 @@ export function getMovies(navigation) {
   const [data, setData] = useState<MovieModel[]>([]);
   const unmounted = useRef(false);
   useEffect(() => {
-    fetch("https://streaming.arthurcargnelli.eu/Playback/BitrateTest?Size=500000&api_key=618ab0c72e18452995c98aa270b8ac75")
-    fetch("https://streaming.arthurcargnelli.eu/Playback/BitrateTest?Size=1000000&api_key=618ab0c72e18452995c98aa270b8ac75")
-    fetch("https://streaming.arthurcargnelli.eu/Playback/BitrateTest?Size=3000000&api_key=618ab0c72e18452995c98aa270b8ac75")
-    fetch("https://streaming.arthurcargnelli.eu/Users/30af1f55f41a40e593194710131bf55d/Items?SortBy=SortName%2CProductionYear&SortOrder=Ascending&IncludeItemTypes=Movie&Recursive=true&Fields=PrimaryImageAspectRatio%2CMediaSourceCount%2CBasicSyncInfo&ImageTypeLimit=1&EnableImageTypes=Primary%2CBackdrop%2CBanner%2CThumb&StartIndex=0&ParentId=f137a2dd21bbc1b99aa5c0f6bf02a805&Limit=100&api_key=618ab0c72e18452995c98aa270b8ac75")
+    fetch("https://streaming.arthurcargnelli.eu/Playback/BitrateTest?Size=500000&api_key=535f213cb7bc4142838e5390270493cb")
+    fetch("https://streaming.arthurcargnelli.eu/Playback/BitrateTest?Size=1000000&api_key=535f213cb7bc4142838e5390270493cb")
+    fetch("https://streaming.arthurcargnelli.eu/Playback/BitrateTest?Size=3000000&api_key=535f213cb7bc4142838e5390270493cb")
+    fetch("https://streaming.arthurcargnelli.eu/Users/bbfb33db95d74eef8761c63b9dd929cb/Items?SortBy=SortName%2CProductionYear&SortOrder=Ascending&IncludeItemTypes=Movie&Recursive=true&Fields=PrimaryImageAspectRatio%2CMediaSourceCount%2CBasicSyncInfo&ImageTypeLimit=1&EnableImageTypes=Primary%2CBackdrop%2CBanner%2CThumb&StartIndex=0&ParentId=db4c1708cbb5dd1676284a40f2950aba&Limit=100&api_key=535f213cb7bc4142838e5390270493cb")
     .then( response => {
       if (response.status == 200) {
         return response.json()
